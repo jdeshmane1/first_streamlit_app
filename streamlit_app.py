8a85b868b26199a4bb5c6a4121450779cd7996eb
jdeshmane1 <118163423+jdeshmane1@users.noreply.github.com>
2022-11-14 14:58:47 +0530

    Update streamlit_app.py
    added breakfast favourites with 3 item and header.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,10 @@
 import streamlit
 
 streamlit.title('My Mom new Healthy Dinner')
-streamlit.header('Breakfast Menu')
-streamlit.text('omega 3 & Blueberry oatmeal')
-streamlit.text('kale,spinach & rocket smoothie')
-streamlit.text('hard boiled free range egg')
+streamlit.header(' Breakfast favourites')
+streamlit.text('🥣 omega 3 & Blueberry oatmeal')
+streamlit.text('🥗 kale,spinach & rocket smoothie')
+streamlit.text('🐔 hard boiled free range egg')
+streamlit.text('🥑 🍞avocado toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
