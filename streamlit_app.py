02e751d7c78bf4c70e7132404d1fa2b306161b89
jdeshmane1 <118163423+jdeshmane1@users.noreply.github.com>
2022-11-14 14:45:06 +0530

    Update streamlit_app.py
    added 3 lines of menus

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,6 @@ import streamlit
 
 streamlit.title('my parents new healthy dinner')
 streamlit.title('Breakfast Menu')
+streamlit.title('omega 3 & oatmeal')
+streamlit.title('kale,spinach & smoothie')
+streamlit.title('hard boiled free range egg')
